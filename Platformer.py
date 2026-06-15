2a898422f92daa73175aad5cee1aba76efd03e8b
Hidden-Ghost-123 <krishna.shree.hari.1008@gmail.com>
2026-06-15 14:48:10 +0100

    Update Platformer.py

--- Platformer.py
+++ Platformer.py
@@ -1,29 +1,69 @@
 # Impossible Platformer Game
-#Nishchay Bhudia
+# Nishchay Bhudia
 
 import pygame
 
 # initialise pygame
 pygame.init()
 
-#display settings
+# display settings
 WIDTH = 1000
 HEIGHT = 800
+FPS = 60
 
-#create window 
+PLAYER_VEL = 5
+
+# create window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Impossible Game")
 
-#main loop
+clock = pygame.time.Clock()
+
+
+# player Class
+class Player:
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = (255, 0, 0)
+
+    def move_left(self, vel):
+        self.rect.x -= vel
+
+    def move_right(self, vel):
+        self.rect.x += vel
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+
+# create the player
+player = Player(100, 100, 50, 50)
+
+# main loop
 run = True
 
 while run:
+
+    clock.tick(FPS)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    window.fill((0, 0, 0))
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+    # draw background
+    window.fill((64, 224, 208))
+
+    # draw player
+    player.draw(window)
 
     pygame.display.update()
 
